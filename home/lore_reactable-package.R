6c7c7e4d725b5fa309b4efef973e8f212abb8558
raupy <github.r02ni@passmail.net>
2024-08-22 20:16:49 +0200

    initial website commit

---
+++ home/lore_reactable-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @import reactable
+#' @import htmltools
+#' @importFrom dplyr select rename mutate
+#' @importFrom readxl read_excel
+#' @importFrom stringr str_replace_all str_remove_all
+#' @importFrom fs path
+
+## usethis namespace: start
+## usethis namespace: end
+#NULL
